dbb63c83934263a913e06e4870f7116d923b7f46
thevoid12 <iamvinod2001@gmail.com>
2025-08-11 11:32:19 +0530

    claude code header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import chefIcon from '../assets/chef-claude-icon.png'
+export default function Header(){
+    return(
+        <header>
+            <img src={chefIcon}/>
+            <h1>Chef claude</h1>
+        </header>
+    )
+}
