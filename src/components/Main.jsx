dc7e9be09f288553dc3cc98a1d7d76761fa02308
thevoid12 <iamvinod2001@gmail.com>
2025-08-12 13:15:04 +0530

    conditional rendering message

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,22 +3,30 @@ import {useState} from 'react'
 export default function Main() {
     const [ingredients,setIngredients]=useState([])
         // const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-        const ingMap = ingredients.map((item,index)=>{ // when state is updated using setIngredients the comp gets reloaded so the imgmap will also get updated in that process
+        const ingredientsListItems = ingredients.map((item,index)=>{ // when state is updated using setIngredients the comp gets reloaded so the imgmap will also get updated in that process
             return <li key={index}>{item}</li>// note that its a bad practice to have key as index
         })
 
-        function handleSubmit(event){
-               event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+        // function handleSubmit(event){
+        //        event.preventDefault()
+        // const formData = new FormData(event.currentTarget)
+        // const newIngredient = formData.get("ingred")
+        // setIngredients(
+        //     prevIngredients => [...prevIngredients,newIngredient]
+        // )
+        // }
+
+          function addIngredients(formData){ // action={} does prevent default (form refreshing ) and clear the input form after submit button is clicked by defualt(we dont have to write it) 
         const newIngredient = formData.get("ingred")
         setIngredients(
             prevIngredients => [...prevIngredients,newIngredient]
         )
+
         }
 
         return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredients} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. tomato"
@@ -28,9 +36,19 @@ export default function Main() {
                 <button >Add ingredient</button>
               
             </form>
-             <ul>
-                {ingMap}
-                </ul>
+               { ingredientsListItems.length>0 &&
+                <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
+            }
         </main>
     )
 }
