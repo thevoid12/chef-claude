1a90b4aad17e697bd278ce0f7af7d54379ad42ce
thevoid12 <iamvinod2001@gmail.com>
2025-08-12 09:20:18 +0530

    updating ingredients array state using useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,36 @@
+import {useState} from 'react'
+
 export default function Main() {
-    return (
+    const [ingredients,setIngredients]=useState([])
+        // const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+        const ingMap = ingredients.map((item,index)=>{ // when state is updated using setIngredients the comp gets reloaded so the imgmap will also get updated in that process
+            return <li key={index}>{item}</li>// note that its a bad practice to have key as index
+        })
+
+        function handleSubmit(event){
+               event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingred")
+        setIngredients(
+            prevIngredients => [...prevIngredients,newIngredient]
+        )
+        }
+
+        return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
-                    placeholder="e.g. oregano"
+                    placeholder="e.g. tomato"
                     aria-label="Add ingredient"
+                    name="ingred"
                 />
-                <button>Add ingredient</button>
+                <button >Add ingredient</button>
+              
             </form>
+             <ul>
+                {ingMap}
+                </ul>
         </main>
     )
 }
